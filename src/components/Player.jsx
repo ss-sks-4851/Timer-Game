d49d8fd1cccc80ead1bfd2d79e4f35a820b27cf1
Shambhavi Shanker <shambhavishanker1627@gmail.com>
2024-05-15 17:41:14 +0530

    initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import {useState, useRef} from "react";
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName,setPlayerName] = useState('');
+  
+  function handleClick(){
+    setPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
+  //?? is shortcut for ternary operator
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2> 
+      <p>
+        <input 
+          ref = {playerName}
+          type="text" 
+        />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
